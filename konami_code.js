fe157b9b610a2c92d5c7c42ef69ba09644121caa
Suchitra Naik <susanaik@gmail.com>
2018-07-14 21:16:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
 let index = 0;
-document.body.addEventListener('keydown', onKeyDownHandler(e)
+document.body.addEventListener('keydown', (e) =>{
 const key = parseInt(e.detail || e.which);
 
 if (key === alphabet[index]) {
@@ -16,5 +16,6 @@ if (key === alphabet[index]) {
   }
 } else {
   index = 0;
+}
 });
 }
